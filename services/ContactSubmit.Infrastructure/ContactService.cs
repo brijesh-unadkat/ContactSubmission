dad6d4a6ab4a8642477cba4f1beb2ea1dcc55828
brijesh-unadkat <brijesh.unadkat@posten.no>
2025-05-09 11:54:18 +0530

    Docker config update and validation commented

--- services/ContactSubmit.Infrastructure/ContactService.cs
+++ services/ContactSubmit.Infrastructure/ContactService.cs
@@ -21,18 +21,18 @@ namespace ContactSubmit.Infrastructure
         public async Task<bool> AddContact(ContactModel addContact)
         {
             //Check for duplicate email id
-            bool isEmailExists = _databaseContext.Contact.Where(x=>x.EmailAddress.ToLower() == addContact.EmailAddress.ToLower())?.FirstOrDefault() != null;
-            bool isPhoneExists = _databaseContext.Contact.Where(x => x.PhoneNumber.ToLower() == addContact.PhoneNumber.ToLower())?.FirstOrDefault() != null;
-            if (isEmailExists || isPhoneExists)
-            {
-                return false;
-            }
-            else
-            {
+            //bool isEmailExists = _databaseContext.Contact.Where(x=>x.EmailAddress.ToLower() == addContact.EmailAddress.ToLower())?.FirstOrDefault() != null;
+            //bool isPhoneExists = _databaseContext.Contact.Where(x => x.PhoneNumber.ToLower() == addContact.PhoneNumber.ToLower())?.FirstOrDefault() != null;
+            //if (isEmailExists || isPhoneExists)
+            //{
+            //    return false;
+            //}
+            //else
+            //{
                 _databaseContext.Contact.Add(addContact);
                 await _databaseContext.SaveChangesAsync();
                 return true;
-            }
+            //}
         }
 
         
